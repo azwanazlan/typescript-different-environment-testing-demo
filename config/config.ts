1cc611218528bdbdd4bea978591fe33dc849a812
azwanazlan <azwanazlan2809@gmail.com>
2023-09-30 09:58:35 +0800

    add gitignore delete nodemodules

--- config/config.ts
+++ config/config.ts
@@ -4,9 +4,9 @@ import fs from 'fs-extra';
 dotenv.config();
 
 const env = process.env.ENV || 'dev';
+
 const configPath = `config/config.${env}.json`;
 
 fs.pathExists(configPath);
 
 export const testConfiguration = fs.readJsonSync(configPath);
-
